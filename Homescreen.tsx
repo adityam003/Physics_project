01433883e6c7e2296fda7f7399f8114f561ac551
Aditya <adityamurali003@gmail.com>
2024-03-03 19:13:55 +0530

    homescreen

--- Homescreen.tsx
+++ Homescreen.tsx
@@ -1,48 +1,47 @@
 import React from 'react';
 import { ImageComponent, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { Button, Image } from 'react-native-elements';
-import { black } from 'react-native-paper/lib/typescript/styles/themes/v2/colors';
-
+import { Button, Image, Tile } from 'react-native-elements';
+import { Paragraph, Title } from 'react-native-paper';
 const Homescreen = () => {
 
   return(
       <SafeAreaView>
-      <ScrollView >       
+          <ScrollView>
       <View>
-        <Text style = {styles.text}> GOOT</Text>
-      </View>
-      <Button style = {{width: 10}}>
-        <Image src={require('./assets/photos/drums.json')}></Image>
-      </Button>
-
+        <Text style={styles.text}> GOOT</Text>
+      </View> 
+     
       <View style = {styles.underlappingCard}/> 
-      <View style = {styles.overlappingCard}/>
-    
-      <View style = {styles.columnContainer}>
-      <View style = {styles.container} >
-      <TouchableOpacity style = {[styles.notesCard,styles.card]} />
-      <TouchableOpacity style = {[styles.todolistCard,styles.card]}/> 
+      <View style = {styles.overlappingCard}>
+        <Title style = {{fontWeight:'bold',fontSize:34, color: 'white', paddingTop:30,paddingLeft:20}}>Community.</Title>
+        <Paragraph style = {{fontWeight: 'bold', fontSize:24 , color: 'white', paddingTop: 34, paddingLeft:20 }}>Explore, Grow, Share</Paragraph>
+        <Paragraph style = {{fontWeight: 'bold', fontSize:24 , color: 'white', paddingTop: 5, paddingLeft:22 }}>together.</Paragraph>
+        <Paragraph style = {{fontWeight: 'bold', fontSize:20 , color: 'white', paddingTop: 43, paddingLeft:170 }}>coming soon...</Paragraph>
       </View>
       <View style = {styles.container}>
+      <TouchableOpacity style = {[styles.notesCard,styles.card]}>
+        {/* <Title style = {{fontWeight: 'bold', fontSize: 24,padding: 15, paddingLeft: 35}}>Notes</Title> */}
+      </TouchableOpacity>
+      <TouchableOpacity style = {[styles.todolistCard, styles.card]}/> 
+     </View>
+      <View style = {styles.row2}>
       <TouchableOpacity style = {[styles.updatesCard,styles.card]}/> 
-      <TouchableOpacity style = {[styles.snippesCard,styles.card]}/> 
+      <TouchableOpacity style = {[styles.snippetsCard,styles.card]}/> 
       </View>
-      
+     </ScrollView>
 
- </View>
-</ScrollView>
+ {/* </View> */}
+<View style = {styles.navbar}/>
+
       </SafeAreaView>
     )
   };
 const styles = StyleSheet.create({
     text: {
-    width : '106%',
-    height : '50%', 
     fontSize: 34,
     fontWeight: 'bold',
-    color: '#000000',
-    paddingLeft: 10,
-    paddingTop: 10
+    color: 'black',
+    padding:10,
     },
   
     underlappingCard:{
@@ -68,44 +67,60 @@ const styles = StyleSheet.create({
   
    columnContainer:{
     flex:1,
-    flexDirection:'column',
+    
+    flexDirection:'row',
     marginTop:30,
    },
     container:{
       flex: 1,
-      flexWrap: 'wrap',
       flexDirection: 'row',
-      justifyContent:'space-between',
-      paddingTop: 20,
-      paddingHorizontal: 24,
-  
+       marginTop:'70%',
+       justifyContent:'center',
+      // margin:10 ,
+    },
+
+    row2:{
+      flex: 1,
+      flexDirection:'row',
+      justifyContent:'center',
+      paddingBottom:70,
     },
      card:{
-      width: 169,
-      height: 234,
+     
+      alignContent:'center',
+      margin:10,
+
+      width: 160,
+      height : 229,
+      //height: 234,
       borderRadius:15,
       elevation:5,
     },
     notesCard:{
-      flex : 1,
-      marginRight: 20,
+     // marginRight: 20,
       backgroundColor: '#F1F58F',
     },  
     todolistCard:{
-      flex : 1,
       backgroundColor: "#FFA930"
     },
     updatesCard:{
-      flex : 1,
-//marginTop: 15,
-      marginRight: 20,
+   
       backgroundColor: '#A9EDF1',
     },
-    snippesCard:{
-      flex : 1,
-     
-    //  marginTop:150,
+  snippetsCard:{
       backgroundColor: '#99FF76'
+    },
+    navbar:{
+      position: "absolute",
+      marginTop: "186%",
+      marginLeft: 12,
+      width: 370,
+      height: 45,
+      borderRadius:15,
+      borderWidth: 1,
+      borderColor:'#BEB7B7',
+      backgroundColor: 'white',
+      elevation: 10,
     }
 
   })
